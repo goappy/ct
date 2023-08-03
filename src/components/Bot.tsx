8e23596776c87eb8c98202d8f580207978e8fad7
alikibao <35171600+alikibao@users.noreply.github.com>
2023-08-03 07:42:47 +0000

    changed main message

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -31,7 +31,7 @@ export type BotProps = {
     fontSize?: number
 }
 
-const defaultWelcomeMessage = 'Hi there! How can I help?'
+const defaultWelcomeMessage = 'Hi I wil be your AI Tutor'
 
 /*const sourceDocuments = [
     {
